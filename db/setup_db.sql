bfb678c15fc5597c96ae75d135d7f159f593cc55
jwbennett <jwbennett@Laptop>
2024-04-17 16:44:42 +0100

    Added primary keys to tables, as per Government documentation

--- db/setup_db.sql
+++ db/setup_db.sql
@@ -1,9 +1,11 @@
 -- DROP DATABASE IF EXISTS mot_results;
-
 CREATE DATABASE mot_results;
 
 \c mot_results
 
+-- CREATE TABLE fact_test_result_for_table_copy AS
+--     SELECT * FROM fact_test_result;
+-- DROP TABLE if exists fact_test_result;
 CREATE TABLE fact_test_result (
     test_id INT,
     vehicle_id INT,
@@ -18,8 +20,12 @@ CREATE TABLE fact_test_result (
     colour VARCHAR,
     fuel_type VARCHAR,
     cylinder_capacity DECIMAL,
-    first_use_date DATE
+    first_use_date DATE,
+    PRIMARY KEY (test_id)
 );
+-- INSERT INTO fact_test_result
+-- SELECT * FROM fact_test_result_for_table_copy;
+-- DROP TABLE fact_test_result_for_table_copy;
 
 CREATE TABLE fact_test_item (
     test_id INT,
@@ -29,7 +35,9 @@ CREATE TABLE fact_test_item (
     dangerous_mark VARCHAR
 );
 
-
+-- CREATE TABLE dim_item_detail_for_table_copy AS
+--     SELECT * FROM dim_item_detail;
+-- DROP TABLE if exists dim_item_detail;
 CREATE TABLE dim_item_detail (
     rfr_id INT,
     test_class_id INT,
@@ -40,27 +48,34 @@ CREATE TABLE dim_item_detail (
     rfr_loc_marker VARCHAR,
     rfr_insp_manual_desc VARCHAR,
     rfr_advisory_text VARCHAR,
-    test_item_set_section_id INT
+    test_item_set_section_id INT,
+    PRIMARY KEY (rfr_id,test_class_id)
 );
+-- INSERT INTO dim_item_detail
+-- SELECT * FROM dim_item_detail_for_table_copy;
+-- DROP TABLE dim_item_detail_for_table_copy;
 
+-- DROP TABLE if exists dim_item_group;
 CREATE TABLE dim_item_group (
     test_item_id INT,
     test_class_id INT,
     parent_id INT,
     test_item_set_section_id INT,
-    item_name VARCHAR
+    item_name VARCHAR,
+    PRIMARY KEY (test_item_id, test_class_id)
 );
 
 CREATE TABLE dim_mdr_fuel_types (
     type_code VARCHAR,
     fuel_type VARCHAR
 );
-
+-- DROP TABLE if exists dim_mdr_rfr_location;
 CREATE TABLE dim_mdr_rfr_location(
     id INT,
     "lateral" VARCHAR,
     longitudinal VARCHAR,
-    vertical VARCHAR
+    vertical VARCHAR,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE dim_mdr_test_outcome(
