c515d29f514555714ff42430e232ce6cd86948a0
jwbennett <jwbennett@Laptop>
2024-04-26 10:51:11 +0100

    Adds first time pass percentage by reg year view

--- db/create_class4_only_result_views.sql
+++ db/create_class4_only_result_views.sql
@@ -19,4 +19,34 @@ ON dim_mdr_fuel_types.type_code = fact_test_class4_result.fuel_type
 GROUP BY year, simplified_fuel_type
 ORDER BY number_of_tests DESC;
 
+
+
+CREATE VIEW first_time_pass_percentage_by_reg_year
+(registration_year, vehicles_from_registration_year, first_time_pass_percentage) 
+AS (
+	WITH passed_first_time AS (
+		SELECT CAST(DATE_PART('year',fact_test_class4_result.first_use_date) AS INT) AS registration_year,
+		COUNT(*) AS first_time_pass_count
+		FROM fact_test_class4_result
+		WHERE test_result = 'P' AND test_type = 'NT'
+		GROUP BY CAST(DATE_PART('year',fact_test_class4_result.first_use_date) AS INT)
+	),
+	first_registered_count_by_year AS (
+		SELECT CAST(DATE_PART('year',fact_test_class4_result.first_use_date) AS INT) AS registration_year,
+		COUNT(*)
+		FROM fact_test_class4_result
+		GROUP BY CAST(DATE_PART('year',fact_test_class4_result.first_use_date) AS INT)
+	) 
+	SELECT first_registered_count_by_year.registration_year,
+	first_registered_count_by_year.count AS vehicles_from_registration_year,
+	ROUND((cast(passed_first_time.first_time_pass_count AS DECIMAL) /cast(first_registered_count_by_year.count AS DECIMAL) * 100),2) AS percentage_of_vehicles_with_first_time_pass
+	FROM
+	passed_first_time
+	RIGHT OUTER JOIN
+	first_registered_count_by_year
+	ON passed_first_time.registration_year = first_registered_count_by_year.registration_year
+	WHERE first_registered_count_by_year.registration_year >= '1990'
+	ORDER BY first_registered_count_by_year.registration_year DESC
+);
+
 --DROP VIEW tests_count_by_fuel_type;
